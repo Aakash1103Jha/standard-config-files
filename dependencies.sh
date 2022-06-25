c2fbce9dbe96bcb2745bbadf855eab6a33bc083d
Aakash Jha <52240895+Aakash1103Jha@users.noreply.github.com>
2022-06-26 01:12:43 +0530

    Add files via upload
    Installs the following dependencies: 
    ```
    Dev dependencies
    @types/cors @types/express @types/node @types/regenerator-runtime @types/node-webpack-externals ts-loader ts-node-dev typescript webpack webpack-cli webpack-node-externals
    
    Dependencies
    cors dotenv express mongoose regenerator-runtime
    ```

---
+++ dependencies.sh
@@ -0,0 +1,5 @@
+echo Installing dependencies
+npm i --save cors dotenv express mongoose regenerator-runtime; 
+
+echo Installing devDependencies
+npm i --save-dev @types/cors @types/express @types/node @types/regenerator-runtime @types/node-webpack-externals ts-loader ts-node-dev typescript webpack webpack-cli webpack-node-externals
