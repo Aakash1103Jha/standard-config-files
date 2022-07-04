fe24ae08cdc923ea5c6fe8ea0d76522f5710ba7d
Aakash Jha <52240895+Aakash1103Jha@users.noreply.github.com>
2022-07-04 12:52:38 +0530

    Update dependencies.sh

--- dependencies.sh
+++ dependencies.sh
@@ -2,4 +2,4 @@ echo Installing dependencies
 npm i --save cors dotenv express mongoose regenerator-runtime; 
 
 echo Installing devDependencies
-npm i --save-dev @types/cors @types/express @types/node @types/regenerator-runtime @types/node-webpack-externals ts-loader ts-node-dev typescript webpack webpack-cli webpack-node-externals
+npm i --save-dev @types/cors @types/express @types/node @types/regenerator-runtime @types/webpack-node-externals template-ejs-loader html-webpack-plugin ts-loader ts-node-dev typescript webpack webpack-cli webpack-node-externals
